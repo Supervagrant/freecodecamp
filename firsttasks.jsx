896ec46672761894764c95eba5205f9377a39214
Klindziuk <kklindziuk@gmail.com>
2024-06-04 15:14:09 +0200

    Third task

--- firsttasks.jsx
+++ firsttasks.jsx
@@ -1,1 +1,9 @@
 const JSX = <div><h1>Header</h1><p>Some text</p><ul><li>One</li><li>Two</li><li>Three</li></ul></div>
+
+const thirdJSX = (
+    <div>
+      <h1>This is a block of JSX</h1>
+      {/* Here some comments */}
+      <p>Here's a subtitle</p>
+    </div>
+  );
